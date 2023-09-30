7aa1aba9ef27956da6781018489f1dd8a436996f
schlanqe <ilhanburcakbas@gmail.com>
2023-09-30 16:13:31 +0300

    disable italics

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,7 @@
 require("rose-pine").setup({
   disable_background = true,
-  disable_float_background = true
+  disable_float_background = true,
+  disable_italics = true
 })
 
 vim.cmd("colorscheme rose-pine")
