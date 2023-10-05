2261662c7e83d9dde9ad3ed23a5a04e4f22a55ca
schlanqe <ilhanburcakbas@gmail.com>
2023-10-05 18:00:33 +0300

    melange...

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,3 @@
-require("rose-pine").setup({
-  disable_background = true,
-  disable_float_background = true,
-  disable_italics = true
-})
-
-vim.cmd("colorscheme rose-pine")
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.cmd("colorscheme melange")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/ilhan/packer.lua
+++ lua/ilhan/packer.lua
@@ -14,7 +14,7 @@ return require('packer').startup(function(use)
       'nvim-tree/nvim-tree.lua',
       requires = {'nvim-tree/nvim-web-devicons'}
     }
-    use {'rose-pine/neovim', as = 'rose-pine'}
+    use {'savq/melange-nvim'}
 
     use {
       'VonHeikemen/lsp-zero.nvim',
