99404f99234d47bbd7097e009056b66301edc7dd
schlanqe <ilhanburcakbas@gmail.com>
2023-11-07 10:15:18 +0300

    serene & typo fix

--- lua/ilhan/packer.lua
+++ lua/ilhan/packer.lua
@@ -17,17 +17,15 @@ return require('packer').startup(function(use)
 
     use ({'rose-pine/neovim', as = 'rose-pine'})
 
+    use 'schlanqe/serene.nvim'
+
     use {
       'VonHeikemen/lsp-zero.nvim',
       branch = 'v3.x',
       requires = {
-        --- Uncomment these if you want to manage LSP servers from neovim
         {'williamboman/mason.nvim'},
         {'williamboman/mason-lspconfig.nvim'},
-
-        -- LSP Support
         {'neovim/nvim-lspconfig'},
-        -- Autocompletion
         {'hrsh7th/nvim-cmp'},
         {'hrsh7th/cmp-nvim-lsp'},
         {'L3MON4D3/LuaSnip'},
