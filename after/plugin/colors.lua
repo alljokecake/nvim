17bd01fd7a7e0c34111a6bc7a8b0992344203e8b
schlanqe <ilhanburcakbas@gmail.com>
2023-12-12 15:00:40 +0300

    the true vimagean

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -24,4 +24,4 @@ vim.cmd("colorscheme gruvbox")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-vim.cmd("highlight LineNr guifg=#4da9f9")
+vim.cmd("highlight LineNr guifg=#5eacd3")
