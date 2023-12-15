b362295f1a88cb001ad6f32a87b577f511b81dc7
schlanqe <ilhanburcakbas@gmail.com>
2023-12-15 10:14:58 +0300

    lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,23 +1,34 @@
 local lsp = require('lsp-zero')
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({buffer = bufnr})
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'rust_analyzer'},
-  handlers = {
-    lsp.default_setup,
-  },
+    ensure_installed = {'rust_analyzer', 'tsserver'},
+    handlers = {
+        lsp.default_setup,
+    },
 })
 
 lsp.set_preferences({
-  suggest_lsp_servers = false,
-  sign_icons = {
-    error = 'E',
-    warn  = 'W',
-    hint  = 'H',
-    info = 'I'
-  }
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn  = 'W',
+        hint  = 'H',
+        info = 'I'
+    }
 })
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+
+end)
