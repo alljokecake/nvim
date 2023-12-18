6bf940a2f17a7180cf68249182413c1f2df3c1f9
schlanqe <ilhanburcakbas@gmail.com>
2023-12-18 22:07:29 +0300

    tree update

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,15 +1,21 @@
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-    side = "right",
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort_by = "case_sensitive",
+    view = {
+        width = 25,
+        side = "right",
+    },
+    renderer = {
+        icons = {
+            show = {
+                file = false,
+                folder = false,
+            },
+        },
+    },
+    filters = {
+        git_ignored = true,
+        dotfiles = false,
+    },
 })
 
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true}) 
