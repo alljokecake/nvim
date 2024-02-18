836e326b4dfc56aff601ef7d072650b8db7d4412
schlanqe <ilhanburcakbas@gmail.com>
2024-02-18 20:19:00 +0300

    feat: split keymaps

--- lua/ilhan/remap.lua
+++ lua/ilhan/remap.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("n", "<C-l>", ':vertical resize +4<CR>')
+vim.keymap.set("n", "<C-k>", ':resize +4<CR>')
+vim.keymap.set("n", "<C-j>", ':resize -4<CR>')
