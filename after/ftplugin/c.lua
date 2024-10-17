f9fbe298b0cfca553a4c63642d3624cb237b9644
alljokecake <ilhanburcakbas@gmail.com>
2024-10-18 00:37:49 +0300

    remove redundant ftplugins

--- after/ftplugin/c.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_set_keymap('n', '<C-b>', ':!gcc % -o %:r<CR>', {})
-vim.api.nvim_set_keymap('n', '<C-r>', ':!./%:r<CR>', {})
--- after/ftplugin/go.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_set_keymap('n', '<C-b>', ':!go run %:r.go<CR>', {})
