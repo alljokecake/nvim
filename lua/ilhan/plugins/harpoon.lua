295b66585503f1eec8e20b95747cb6ed536196fb
alljokecake <ilhanburcakbas@gmail.com>
2025-08-01 16:46:30 -0700

    couldn't fix tmux utf8 issues, so here is a remap

--- lua/ilhan/plugins/harpoon.lua
+++ lua/ilhan/plugins/harpoon.lua
@@ -14,7 +14,7 @@ return {
 
         vim.keymap.set("n", "<C-o>", function() ui.nav_file(1) end)
         vim.keymap.set("n", "<C-p>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-ğ>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-ü>", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<leader>ğ", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "<leader>ü", function() ui.nav_file(4) end)
     end
 }
