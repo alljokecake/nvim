65136d2b46655f9613fb0f0f70dabf8e573aa97c
alljokecake <ilhan@duck.com>
2026-08-01 18:02:21 -0700

    feat(telescope): add native fzf support

--- lua/ilhan/plugins/telescope.lua
+++ lua/ilhan/plugins/telescope.lua
@@ -2,7 +2,8 @@ return {
     "nvim-telescope/telescope.nvim",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
 
     config = function()
