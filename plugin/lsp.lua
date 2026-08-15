7b70e30ae47483481e41aa4f2b2a17264667ab1a
alljokecake <ilhanburcakbas@gmail.com>
2026-08-14 19:05:02 -0700

    feat(lsp): add julia formatter

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,56 +1,58 @@
 require("fidget").setup({})
 
 require("conform").setup({
-    format_on_save = {
-        timeout_ms = 5000,
-        lsp_format = "fallback",
-    },
-    formatters_by_ft = {
-        lua = { "stylua" },
-        nix = { "nixfmt" },
-        rust = { "rustfmt" },
-        go = { "gofmt" },
-    },
+	format_on_save = {
+		timeout_ms = 5000,
+		lsp_format = "fallback",
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "nixfmt" },
+		rust = { "rustfmt" },
+		go = { "gofmt" },
+		julia = { "runic" },
+	},
 })
 
-
 vim.lsp.enable({
-    "lua_ls",
-    "nixd",
-    "rust_analyzer",
-    "gopls",
-    "julials",
+	"lua_ls",
+	"nixd",
+	"rust_analyzer",
+	"gopls",
+	"julials",
 })
 
 vim.diagnostic.config({
-    virtual_text = true,
-    float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-    },
+	virtual_text = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, opts)
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, opts)
 
 		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-		if client:supports_method('textDocument/completion') then
+		if client:supports_method("textDocument/completion") then
 			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
-			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			local chars = {}
+			for i = 32, 126 do
+				table.insert(chars, string.char(i))
+			end
 			client.server_capabilities.completionProvider.triggerCharacters = chars
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
-
-    end,
+	end,
 })
